054d752a0eaded851fc5fcf4d5ab2bad8fb47a79
fazza-tech <ebranfazza@gmail.com>
2025-01-07 23:24:45 +0530

    added sum and multiply files

---
+++ calculate/sum.js
@@ -0,0 +1,8 @@
+  function calculateSum(a,b){
+    let sum = a+b;
+    console.log(sum);
+}
+
+// module.exports.x = x;
+// module.exports.calculateSum = calculateSum 
+module.exports = {calculateSum}   // Exporting the function calculateSum 
