b84d4d0ed77a3d2e92c5dc960644c7e08c2fe17d
fazza-tech <ebranfazza@gmail.com>
2025-01-10 06:52:07 +0530

    added new file

--- app.js
+++ app.js
@@ -3,6 +3,21 @@
 // const { x, calculateSum } = require('./calculate/sum.js');
 // const {calculateMultiply} = require('./calculate/multiply.js');
 
+const fs = require('fs');
+const https = require('https');
+
+fs.readFile("./file.txt","utf8",(err,data)=>{
+    console.log("Your data is:: "+data);
+})
+
+https.get("https://dummyjson.com/api/products/1",(res)=>{
+    console.log("Fetched data successfully");
+})
+
+setTimeout(()=>{
+    console.log("After 5 seconds it will print");
+},5000)
+
 const {calculateSum, calculateMultiply} = require('./calculate/index.js');
 const data = require('./data.json');
 console.log(data);
@@ -10,4 +25,5 @@ console.log(data);
 calculateSum(2,3);
 
 calculateMultiply(4,0)
+
 
