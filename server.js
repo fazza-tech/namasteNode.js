f55325f12eea7bac303f0ee1cbe581f77c2368b9
fazza-tech <ebranfazza@gmail.com>
2025-01-12 20:17:18 +0530

    additional route added

--- server.js
+++ server.js
@@ -3,8 +3,13 @@
 const http = require("http")
 
 const server = http.createServer((req,res)=>{
+    if(req.url === "/getSecret"){
+        res.end("There is no secret")
+    }
     res.end("Hello world")
 })
 
-server.listen(3000)
+server.listen(3000,()=>{
+    console.log('server is running');
+})
 //waiting for a req
